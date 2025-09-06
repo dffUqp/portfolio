b6433e011c675315bf8d233da22dc2ec865915e7
dffuqp <yuriivasilchuk.work@gmail.com>
2025-09-06 22:34:56 +0300

    feat(tabs): update design of the component

--- src/shared/ui/tabs/tabs.tsx
+++ src/shared/ui/tabs/tabs.tsx
@@ -1,4 +1,11 @@
-import { forwardRef, ReactNode, useEffect, useRef, useState } from 'react';
+import {
+  forwardRef,
+  KeyboardEvent,
+  ReactNode,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 
 import { cn } from 'shared/lib';
 
@@ -61,16 +68,47 @@ const Tabs = forwardRef<HTMLDivElement, TabsProps>(
       container.style.clipPath = `inset(0 ${clipRightPercent}% 0 ${clipLeftPercent}%)`;
     }, [selectedKey]);
 
+    const handleKeyDown = (
+      event: KeyboardEvent<HTMLButtonElement>,
+      index: number,
+    ) => {
+      if (!tabListRef.current) return;
+
+      const focusCandidates = [...tabListRef.current.children] as HTMLElement[];
+
+      switch (event.key) {
+        case 'ArrowLeft':
+          moveFocusToTab(focusCandidates, index - 1);
+          break;
+
+        case 'ArrowRight':
+          moveFocusToTab(focusCandidates, index + 1);
+          break;
+
+        case 'Home':
+          moveFocusToTab(focusCandidates, 0);
+          break;
+
+        case 'End':
+          moveFocusToTab(focusCandidates, focusCandidates.length - 1);
+          break;
+
+        default:
+          break;
+      }
+    };
+
+    const tabsControlsClassName =
+      'w-full flex border border-gray-500 rounded-2xl overflow-hidden';
+
+    const getTabsControlClassName = (isLast: boolean) => ({
+      'w-full cursor-pointer p-2': true,
+      'border-r border-gray-500': !isLast,
+    });
+
     return (
-      <div
-        ref={ref}
-        className="w-full border border-gray-500 rounded-xl overflow-hidden relative"
-      >
-        <div
-          role="tablist"
-          ref={tabListRef}
-          className="w-full flex border-b border-gray-500"
-        >
+      <div ref={ref} className="flex flex-col gap-4 w-full relative">
+        <div role="tablist" ref={tabListRef} className={tabsControlsClassName}>
           {items.map((item, index) => {
             const isLast = items.length - 1 === index;
             const isSelected = selectedKey === item.key;
@@ -78,9 +116,7 @@ const Tabs = forwardRef<HTMLDivElement, TabsProps>(
             return (
               <button
                 ref={selectedKey === item.key ? activeTabElementRef : null}
-                className={cn('w-full cursor-pointer p-2 text-gray-400', {
-                  'border-r border-gray-500': !isLast,
-                })}
+                className={cn(getTabsControlClassName(isLast), 'text-gray-400')}
                 key={item.label}
                 role="tab"
                 type="button"
@@ -88,37 +124,7 @@ const Tabs = forwardRef<HTMLDivElement, TabsProps>(
                 aria-controls={`${item.key}-content`}
                 tabIndex={isSelected ? 0 : -1}
                 aria-selected={isSelected}
-                onKeyDown={event => {
-                  if (!tabListRef.current) return;
-
-                  const focusCandidates = [
-                    ...tabListRef.current.children,
-                  ] as HTMLElement[];
-
-                  switch (event.key) {
-                    case 'ArrowLeft':
-                      moveFocusToTab(focusCandidates, index - 1);
-                      break;
-
-                    case 'ArrowRight':
-                      moveFocusToTab(focusCandidates, index + 1);
-                      break;
-
-                    case 'Home':
-                      moveFocusToTab(focusCandidates, 0);
-                      break;
-
-                    case 'End':
-                      moveFocusToTab(
-                        focusCandidates,
-                        focusCandidates.length - 1,
-                      );
-                      break;
-
-                    default:
-                      break;
-                  }
-                }}
+                onKeyDown={event => handleKeyDown(event, index)}
                 onClick={() => {
                   setSelectedKey(item.key);
                   onChange?.(item.key);
@@ -133,7 +139,10 @@ const Tabs = forwardRef<HTMLDivElement, TabsProps>(
         <div
           aria-hidden
           ref={containerRef}
-          className="absolute top-0 w-full flex border-b border-gray-500 transition-[clip_path] duration-[0.25s]"
+          className={cn(
+            tabsControlsClassName,
+            'absolute top-0 transition-[clip_path] duration-[0.25s]',
+          )}
         >
           {items.map((item, index) => {
             const isLast = items.length - 1 === index;
@@ -143,10 +152,8 @@ const Tabs = forwardRef<HTMLDivElement, TabsProps>(
                 type="button"
                 tabIndex={-1}
                 className={cn(
-                  'w-full cursor-pointer p-2 text-white bg-gray-300/40 pointer-events-none',
-                  {
-                    'border-r border-gray-500': !isLast,
-                  },
+                  getTabsControlClassName(isLast),
+                  'text-white bg-gray-300/40 pointer-events-none',
                 )}
                 key={item.label}
               >
@@ -161,7 +168,7 @@ const Tabs = forwardRef<HTMLDivElement, TabsProps>(
             key={item.key}
             tabIndex={0}
             role="tabpanel"
-            className="p-4"
+            className="rounded-2xl border overflow-hidden border-gray-500"
             id={`${item.key}-content`}
             aria-labelledby={`${item.key}-trigger`}
             hidden={selectedKey !== item.key}
