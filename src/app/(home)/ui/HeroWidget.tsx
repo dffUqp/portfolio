fc1a4096a379c3388c1681601bdaeb76341d19ec
dffuqp <yuriivasilchuk.work@gmail.com>
2025-09-18 14:25:27 +0300

    fix(HeroWidget): chrome preserves wrong scroll position

--- src/app/(home)/ui/HeroWidget.tsx
+++ src/app/(home)/ui/HeroWidget.tsx
@@ -10,21 +10,20 @@ import { getFadeInUpAnimation } from 'utils/animation';
 const HeroWidget = () => {
   return (
     <Container>
-      <div
-        className="flex flex-col animate-in fade-in slide-in-from-bottom-[2%] [animation-duration:500ms] relative z-50 w-full 
-       justify-between text-white md:flex-row gap-6"
+      <motion.div
+        {...getFadeInUpAnimation(0)}
+        className="relative flex flex-col z-50 w-full justify-between text-white md:flex-row gap-6"
       >
         <div className="h-screen md:sticky top-0 flex justify-center items-center">
           <Summary />
         </div>
 
-        <motion.div
-          {...getFadeInUpAnimation(3)}
-          className="relative basis-[60%] xl:basis-[50%]"
-        >
-          <Experience />
-        </motion.div>
-      </div>
+        <div className="basis-[60%] xl:basis-[calc(50%_+_24px)] overflow-y-hidden pl-6">
+          <motion.div className="relative" {...getFadeInUpAnimation(2)}>
+            <Experience />
+          </motion.div>
+        </div>
+      </motion.div>
     </Container>
   );
 };
